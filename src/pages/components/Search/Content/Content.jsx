eecf588f94e1f528ad804b9c375e5988fd904e64
huy <huy@fagmann.no>
2024-11-04 20:13:24 +0100

    conditional rendering

--- src/pages/components/Search/Content/Content.jsx
+++ src/pages/components/Search/Content/Content.jsx
@@ -2,7 +2,26 @@ import React from "react";
 
 const Content = ({ data }) => {
 	console.log("from content", data);
-	return <div>Content</div>;
+
+	// Destructuring the data object
+	const { registrering } = data;
+
+	const regStatus =
+		registrering.registreringsstatus.kodeBeskrivelse.toLowerCase();
+
+	return (
+		<div className="my-8">
+			<div
+				className={`${
+					regStatus === "registrert"
+						? "bg-[#e8f3e9] border-[#158925]"
+						: "bg-[#e6f1f5] border-[#077197]"
+				} border-2 my-6 px-8 py-6 border-rounded-md`}
+			>
+				<div>Kjøretøyet er {regStatus}</div>
+			</div>
+		</div>
+	);
 };
 
 export default Content;
