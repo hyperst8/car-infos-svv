107c33b87422acead7e9f7141f37dfc460c45c65
huy <huy@fagmann.no>
2024-11-04 21:25:59 +0100

    Add data from API to Accordion

--- src/pages/components/Content/Content.jsx
+++ src/pages/components/Content/Content.jsx
@@ -5,10 +5,24 @@ const Content = ({ data }) => {
 	console.log("from content", data);
 
 	// Destructuring the data object
-	const { registrering, periodiskKjoretoyKontroll } = data;
+	const {
+		registrering,
+		periodiskKjoretoyKontroll,
+		kjoretoyId,
+		godkjenning,
+		forstegangsregistrering,
+	} = data;
 
 	const regStatus =
 		registrering.registreringsstatus.kodeBeskrivelse.toLowerCase();
+
+	const forsteRegNorge = forstegangsregistrering.registrertForstegangNorgeDato;
+
+	const forsteReg = godkjenning.forstegangsGodkjenning.forstegangRegistrertDato;
+
+	const tekniskGodkjenning = godkjenning.tekniskGodkjenning;
+
+	const tekniskeData = tekniskGodkjenning.tekniskeData;
 
 	return (
 		<div className="my-8">
@@ -21,14 +35,47 @@ const Content = ({ data }) => {
 			>
 				<div>Kjøretøyet er {regStatus}</div>
 			</div>
+			<h2 className="uppercase font-bold text-center text-2xl mb-8">
+				{kjoretoyId.kjennemerke} {tekniskeData.generelt.merke[0].merke}{" "}
+				{tekniskeData.generelt.handelsbetegnelse[0]}
+			</h2>
 			<Accordion title="EU-kontroll">
 				<p className="font-bold">Sist godkjent</p>
 				<span>{periodiskKjoretoyKontroll.sistGodkjent}</span>
-				<p className="font-bold">Neste frist for godkjent EU-kontroll</p>
+				{periodiskKjoretoyKontroll.kontrollfrist && (
+					<p className="font-bold">Neste frist for godkjent EU-kontroll</p>
+				)}
 				<span>{periodiskKjoretoyKontroll.kontrollfrist}</span>
 			</Accordion>
-			<Accordion title="Additional Vehicle Details">
-				<p>Here you can add additional details about the vehicle.</p>
+			<Accordion title="Registreringsdata">
+				<p className="font-bold">Kjøretøyets status</p>
+				<span>{regStatus}</span>
+				<p className="font-bold">Registreringsnummer</p>
+				<span>{kjoretoyId.kjennemerke}</span>
+				<p className="font-bold">Understellsnummer</p>
+				<span>{kjoretoyId.understellsnummer}</span>
+				<p className="font-bold">Kjøretøygruppe</p>
+				<span>{tekniskGodkjenning.kjoretoyklassifisering.beskrivelse}</span>
+				<p className="font-bold">Første gang registrert</p>
+				<span>{forsteReg}</span>
+				<p className="font-bold">Første gang registrert i Norge</p>
+				<span>{forsteRegNorge}</span>
+				<p className="font-bold">Antall seter</p>
+				<span>{tekniskeData.persontall.sitteplasserTotalt}</span>
+			</Accordion>
+			<Accordion title="Tekniske data">
+				<p className="font-bold">Egen vekt</p>
+				<span>{tekniskeData.vekter.egenvekt} kg</span>
+				<p className="font-bold">Nyttelast</p>
+				<span>{tekniskeData.vekter.nyttelast} kg</span>
+				<p className="font-bold">Tillatt total vekt</p>
+				<span>{tekniskeData.vekter.tillattTotalvekt} kg</span>
+				<p className="font-bold">Bredde</p>
+				<span>{tekniskeData.dimensjoner.bredde / 10} cm</span>
+				<p className="font-bold">Høyde</p>
+				<span>{tekniskeData.dimensjoner.hoyde / 10} cm</span>
+				<p className="font-bold">Lengde</p>
+				<span>{tekniskeData.dimensjoner.lengde / 10} cm</span>
 			</Accordion>
 		</div>
 	);
